99276ac9c9e5ebcd2937200a5b9aabce7b937d51
zanenkn <zane.neikena@gmail.com>
2019-03-20 16:06:28 +0100

    passes test: returns fizz if number is divisable by 3

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,6 +5,9 @@ describe "fizz_buzz" do
         expect(fizz_buzz(1)).to eq 1
     end
 
+    it 'returns fizz if number is divisable by 3' do
+        expect(fizz_buzz(3)).to eq "fizz"
+    end
 
 
 end
