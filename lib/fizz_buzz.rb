cbcaeec093ba157722f67fb2088f021c762aa1c0
zanenkn <zane.neikena@gmail.com>
2019-03-20 16:40:24 +0100

    passes the happy path tests

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,16 @@
 def fizz_buzz(number)
-    if number %3 == 0
+    if number %3 == 0 && number %5 == 0
+    'fizzbuzz'
+    
+    elsif number %3 == 0
         'fizz'
+
+    elsif number %5 == 0
+        'buzz'
+    
     else
         number
     end
-    
+
+
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -9,5 +9,12 @@ describe "fizz_buzz" do
         expect(fizz_buzz(3)).to eq "fizz"
     end
 
+    it 'returns buzz if number is divisable by 5' do
+        expect(fizz_buzz(5)).to eq "buzz"
+    end
+
+    it 'returns fizzbuzz if number is divisable by 15' do
+        expect(fizz_buzz(15)).to eq "fizzbuzz"
+    end
 
 end
